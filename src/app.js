db76758cd8e8527a0a2485443bbdef337fb117e3
Piyush Bachani <Piyush>
2023-10-20 15:23:45 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -26,7 +26,15 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for(let i = 0 ; i < PLAYERS.length ; i++ ){
+        let playerinfo = {
+            nameofplayer:PLAYERS[i],
+            strengthofplayer:getRandomStrength(),
+            imageofplayer:"images/super-"+(i+1)+".png",
+            typeofplayer: i % 2 === 0 ? "hero" : "villain"
+        };
+        detailedPlayers.push(playerinfo);
+    }
     return detailedPlayers;
 }
 
@@ -34,6 +42,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.floor(Math.random()*100)
 }
 
 const buildPlayers = (players, type) => {
@@ -42,7 +51,11 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+    for(let i = 0 ; i < players.length; i++){
+        if (players[i].typeofplayer === type ){
+            fragment += `<div class="player"><img src="${players[i].imageofplayer}"><h2>${players[i].nameofplayer}</h2><p>Strength:${players[i].strengthofplayer}</p></div>`
+        }
+    }
     return fragment;
 }
 // Display players in HTML
